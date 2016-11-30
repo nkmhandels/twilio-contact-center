840623a6183dd8994c4ff70ecb39ca8725f7072c
nkmhandels <info@nkmhandels.de>
2016-11-30 14:14:39 +0100

    Update ivr.js

--- controllers/ivr.js
+++ controllers/ivr.js
@@ -39,7 +39,7 @@ module.exports.selectTeam = function (req, res) {
 	/* the caller pressed a key that does not match any team */
 	if (team === null) {
 		// redirect the call to the previous twiml
-		twiml.say(['voice' => 'alice', 'language' => 'en-GB'], 'Your selection was not valid, please try again')
+		twiml.say({voice:alice, language:en-GB}, 'Your selection was not valid, please try again')
 				twiml.pause({length: 2})
 		twiml.redirect({ method: 'GET' }, 'welcome')
 	} else {
